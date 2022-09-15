c45ea2db0936ab9f94b3e17b902d95a9f7769c0f
SparrowH <111083793+SparrowH@users.noreply.github.com>
2022-09-15 05:46:43 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-# include "main.h"
+#include "main.h"
 
 /**
  * print_alphabet - Entry point
@@ -7,11 +7,14 @@
 
 void print_alphabet(void)
 {
-char c = 'a';    
-while (c <= 'z')
-{
- _putchar(c);
- c++;
- }
-_putchar('\n');
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
 }
